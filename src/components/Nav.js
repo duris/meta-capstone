f7daf97021501ca0da17b4540be1423b09f06c26
Ross <ross@duris.io>
2023-01-19 09:12:28 -0500

    Added hamburger icon and mobile adjustments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,22 @@
+import hamburger from '../icons/mobile.svg'
+
 const Nav = () => {
   return(
-    <nav>
+    <>
+    <nav class='mobile'>
+      <div className='burger'>
+        <img src={hamburger} width={80} height={20} />
+      </div>
+      <ul>
+        {/* <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Reservations</a></li>
+        <li><a href="#">Order Online</a></li>
+        <li><a href="#">Login</a></li> */}
+      </ul>
+    </nav>
+    <nav class='not-mobile'>
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
@@ -10,6 +26,7 @@ const Nav = () => {
         <li><a href="#">Login</a></li>
       </ul>
     </nav>
+    </>
   )
 }
 
